7c126b51b7952c8c8a6cd9f31740c0a2a7d8353a
Mukhammad <nomonhojaevm@gmail.com>
2026-06-11 13:27:35 +0900

    feat: develop socket.io client

--- src/app/context/ContextProvider.tsx
+++ src/app/context/ContextProvider.tsx
@@ -12,7 +12,6 @@ const ContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
 	);
 	const [orderBuilder, setOrderBuilder] = useState<Date>(new Date());
 
-	console.log("=== verify ===");
 	return (
 		<GlobalContext.Provider
 			value={{ authMember, setAuthMember, orderBuilder, setOrderBuilder }}>
--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,10 @@ import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
-import "./css/index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import ContextProvider from "./app/context/ContextProvider";
+import { SocketProvider } from "./app/context/SocketContext";
+import "./css/index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -19,12 +20,14 @@ root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<ContextProvider>
+				<SocketProvider>
 				<ThemeProvider theme={theme}>
 					<CssBaseline />
 					<Router>
 						<App />
 					</Router>
 				</ThemeProvider>
+				</SocketProvider>
 			</ContextProvider>
 		</Provider>
 	</React.StrictMode>,
